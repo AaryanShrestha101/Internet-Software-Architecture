49b7749a073597b07644ea2798bda76e445e1d68
Aaryan Shrestha <np03cs4a220142@heraldcollege.edu.np>
2023-04-04 17:01:50 +0545

    Weather App update

--- WeatherApp/index.js
+++ WeatherApp/index.js
@@ -1,75 +1,102 @@
+// Select HTML elements
 const container = document.querySelector('.container');
 const search = document.querySelector('.search-box button');
 const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
+const cityInput = document.querySelector('.search-box input');
 
-search.addEventListener('click', () => {
+// Default city
+const defaultCity = "anniston";
 
-    const APIKey = '0168c6ccf28216393e499c474a3a0ff5';
-    const city = document.querySelector('.search-box input').value;
-    if (city === '')
+// Function to fetch weather data
+function getWeatherData(city) {
+  // API key
+  const APIKey = '0168c6ccf28216393e499c474a3a0ff5';
+
+  // Check if city input is empty
+  if (city === '') {
+    return;
+  }
+
+  // Fetch weather data from API
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+    .then(response => response.json())
+    .then(json => {
+
+      // If city not found, display error message
+      if (json.cod === '404') {
+        container.style.height = '400px';
+        weatherBox.style.display = 'none';
+        weatherDetails.style.display = 'none';
+        error404.style.display = 'block';
+        error404.classList.add('fadeIn');
         return;
-    
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
-        .then(response => response.json())
-        .then(json => {
+      }
 
-            if (json.cod === '404') {
-                container.style.height = '400px';
-                weatherBox.style.display = 'none';
-                weatherDetails.style.display = 'none';
-                error404.style.display = 'block';
-                error404.classList.add('fadeIn');
-                return;
-            }
+      // Hide error message
+      error404.style.display = 'none';
+      error404.classList.remove('fadeIn');
 
-            error404.style.display = 'none';
-            error404.classList.remove('fadeIn');
+      // Extract weather data from JSON
+      const image = document.querySelector('.weather-box img');
+      const temperature = document.querySelector('.weather-box .temperature');
+      const description = document.querySelector('.weather-box .description');
+      const humidity = document.querySelector('.weather-details .humidity span');
+      const wind = document.querySelector('.weather-details .wind span');
+      const pressure = document.querySelector('.weather-details .pressure span')
 
-            const image = document.querySelector('.weather-box img');
-            const temperature = document.querySelector('.weather-box .temperature');
-            const description = document.querySelector('.weather-box .description');
-            const humidity = document.querySelector('.weather-details .humidity span');
-            const wind = document.querySelector('.weather-details .wind span');
+      switch (json.weather[0].main) {
+        case 'Clear':
+          image.src = 'images/clear-day.svg';
+          break;
 
-            switch (json.weather[0].main) {
-                case 'Clear':
-                    image.src = 'images/clear.png';
-                    break;
+        case 'Rain':
+          image.src = 'images/rain.svg';
+          break;
 
-                case 'Rain':
-                    image.src = 'images/rain.png';
-                    break;
+        case 'Snow':
+          image.src = 'images/snow.svg';
+          break;
 
-                case 'Snow':
-                    image.src = 'images/snow.png';
-                    break;
+        case 'Clouds':
+          image.src = 'images/cloudy.svg';
+          break;
 
-                case 'Clouds':
-                    image.src = 'images/cloud.png';
-                    break;
+        case 'Haze':
+          image.src = 'images/mist.svg';
+          break;
 
-                case 'Haze':
-                    image.src = 'images/mist.png';
-                    break;
+        default:
+          image.src = '';
+      }
 
-                default:
-                    image.src = '';
-            }
+      temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+      description.innerHTML = `${json.weather[0].description}`;
+      humidity.innerHTML = `${json.main.humidity}%`;
+      wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+      pressure.innerHTML = `${parseInt(json.main.pressure)}`
 
-            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
-            description.innerHTML = `${json.weather[0].description}`;
-            humidity.innerHTML = `${json.main.humidity}%`;
-            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
-            ren.innerHTML = `${parseInt(json.rainVolume)} mm`;
+      // Show weather box and details
+      weatherBox.style.display = '';
+      weatherDetails.style.display = '';
+      weatherBox.classList.add('fadeIn');
+      weatherDetails.classList.add('fadeIn');
+      container.style.height = '590px';
+    });
+}
 
-            weatherBox.style.display = '';
-            weatherDetails.style.display = '';
-            weatherBox.classList.add('fadeIn');
-            weatherDetails.classList.add('fadeIn');
-            container.style.height = '590px';   
-            city.value = "anniston";
-        });
-        search.click();
+// Event listener for search button click
+search.addEventListener('click', () => {
+  const city = cityInput.value;
+  getWeatherData(city);
+});
+
+// Event listener for window load
+window.addEventListener('load', () => {
+  // Set default city value
+  cityInput.placeholder = defaultCity;
+
+  // Get weather data for default city
+  getWeatherData(defaultCity);
 });
